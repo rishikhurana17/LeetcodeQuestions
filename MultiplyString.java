a437777309de2f4dcd8e174ff91ca0ffc60491a4
rishikhurana17 <rishikhurana17@gmail.com>
2018-11-12 00:04:50 -0800

    fixed code

--- MultiplyString.java
+++ MultiplyString.java
@@ -4,24 +4,24 @@ package leetcode;
  * Created by rkhurana on 10/25/18.
  */
 public class MultiplyString {
-    public static String multiply(String a, String b) {
+    public static String multiply(String num1, String num2) {
+        int[] sum = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int carry = 0;
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int tmp = (sum[i + j + 1]) + (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + carry;
+                sum[i + j + 1] = tmp % 10;
+                carry = tmp / 10;
+            }
+            sum[i] += carry;
+        }
 
+        // below lines are only for trimming zero in the beginning of the number
         StringBuilder sb = new StringBuilder();
-    int i = a.length() - 1, j = b.length() - 1, carry = 0;
-		while (i >= 0 || j >= 0) {
-        int sum = carry;
-//			if (j >= 0)
-//				sum += b.charAt(j--) - '0';
-        // '0' is for converting the char to its actual int value, otherwise the returned int is its ascii value
-//			if (i >= 0)
-        sum += (a.charAt(i--) - '0') * (b.charAt(j--) - '0');
-        sb.append(sum % 10);
-        carry = sum / 10;
-    }
-		if (carry != 0)
-            sb.append(carry);
-		return sb.reverse().toString();
-
+        for (int i : sum)
+            if (sb.length() > 0 || i > 0)
+                sb.append(i);
+        return (sb.length() == 0) ? "0" : sb.toString();
     }
     public static void main(String []args){
         System.out.println(multiply("9","99"));
